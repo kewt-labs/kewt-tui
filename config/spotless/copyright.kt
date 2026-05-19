e2b2744a4a9d353fec148e226fc60670aca3799f
lscythe <lscythe.git@proton.me>
2026-05-19 13:56:13 +0700

    chore: update copyright headers to "Kewt Labs" across all files

--- config/spotless/copyright.kt
+++ config/spotless/copyright.kt
@@ -1,5 +1,5 @@
 /*
-* Copyright $YEAR lscythe
+* Copyright $YEAR Kewt Labs
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
--- kewt-terminal/src/commonMain/kotlin/dev/kewt/terminal/AnsiTerminal.kt
+++ kewt-terminal/src/commonMain/kotlin/dev/kewt/terminal/AnsiTerminal.kt
@@ -1,5 +1,5 @@
 /*
-* Copyright 2026 lscythe
+* Copyright 2026 Kewt Labs
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
